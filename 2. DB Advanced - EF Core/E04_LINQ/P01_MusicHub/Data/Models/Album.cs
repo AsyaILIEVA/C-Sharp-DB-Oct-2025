e9ddce0725ff329be184c7f59a34073600b0ed14
ASYA <asyailieva@yahoo.com>
2025-10-31 00:13:43 +0000

    Added problem solution to Exercise LINQ for P01 MusicHub
    Added problem solution to Exercise LINQ for P01 MusicHub

---
+++ 2. DB Advanced - EF Core/E04_LINQ/P01_MusicHub/Data/Models/Album.cs
@@ -0,0 +1,29 @@
+﻿namespace MusicHub.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using static Common.EntityValidation.Album;
+
+    public class Album
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public DateTime ReleaseDate { get; set; }
+
+        [NotMapped]
+        public decimal Price => this.Songs.Sum(s => s.Price);
+
+        [ForeignKey(nameof(Producer))]
+        public int? ProducerId { get; set; }
+        public virtual Producer? Producer { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+                = new HashSet<Song>();
+
+    }
+}
