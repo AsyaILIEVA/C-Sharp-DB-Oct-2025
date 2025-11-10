80dd4d67e4f3ddd9c355709db64e622e93e14efb
ASYA <asyailieva9@gmail.com>
2025-11-10 18:16:26 +0000

    Added solution CarDealer to JSON Exercise
    Added solution CarDealer to JSON Exercise _ 09.Import Suppliers

---
+++ 2. DB Advanced - EF Core/E07_JavaScript Object Notation - JSON/CarDealer/DTOs/Import/ImportSupplierDto.cs
@@ -0,0 +1,16 @@
+﻿namespace CarDealer.DTOs.Import
+{
+    using Newtonsoft.Json;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ImportSupplierDto
+    {
+        [Required]
+        [JsonProperty("name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [JsonProperty("isImporter")]
+        public string IsImporter { get; set; } = null!;
+    }
+}
---
+++ 2. DB Advanced - EF Core/E07_JavaScript Object Notation - JSON/CarDealer/StartUp.cs
@@ -0,0 +1,76 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Xml;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using CarDealerContext dbContext = new CarDealerContext();
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+
+            // Console.WriteLine(Directory.GetCurrentDirectory());
+            string jsonFileDirPath = Path
+                .Combine(Directory.GetCurrentDirectory(), "../../../Datasets/");
+            string jsonFileName = "suppliers.json";
+            string jsonFileText = File
+                .ReadAllText(jsonFileDirPath + jsonFileName);
+
+            string result = ImportSuppliers(dbContext, jsonFileText);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            ICollection<Supplier> suppliersToImport = new List<Supplier>();
+
+            IEnumerable<ImportSupplierDto>? supplierDtos = JsonConvert
+                .DeserializeObject<ImportSupplierDto[]>(inputJson);
+            if (supplierDtos != null)
+            {
+                foreach (ImportSupplierDto supplierDto in supplierDtos)
+                {
+                    if (!IsValid(supplierDto))
+                    {
+                        continue;
+                    }
+
+                    bool isImporterValidVal=bool
+                        .TryParse(supplierDto.IsImporter, out bool isImporter);
+                    if (!isImporterValidVal)
+                    {
+                        continue;
+                    }
+                    Supplier newSupplier = new Supplier()
+                    {
+                        Name = supplierDto.Name,
+                        IsImporter = isImporter
+                    };
+                    suppliersToImport.Add(newSupplier);
+                }
+
+                context.Suppliers.AddRange(suppliersToImport);
+                context.SaveChanges();
+            }
+
+            return $"Successfully imported {suppliersToImport.Count}.";
+        }
+
+        private static bool IsValid(object obj)
+        {
+            ValidationContext validationContext = new ValidationContext(obj);
+            ICollection<ValidationResult> validationResults 
+                = new List<ValidationResult>();
+
+            return Validator
+                .TryValidateObject(obj, validationContext, validationResults);
+        }
+    }
+}
